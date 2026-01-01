d5a5a93b5da22c80b3b925959e636b52ba25de49
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2026-01-01 12:54:20 +0800

    Update build.sh

--- armsr-armv8/build.sh
+++ armsr-armv8/build.sh
@@ -64,7 +64,7 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+# PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
