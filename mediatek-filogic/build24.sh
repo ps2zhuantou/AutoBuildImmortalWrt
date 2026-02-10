b7adad5d466e0847a05b4fb51dad23f56312c1dc
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2026-02-10 13:52:32 +0800

    Add bash and dawn packages to installation list

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -46,12 +46,13 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
-PACKAGES="$PACKAGES curl luci luci-i18n-base-zh-cn"
+PACKAGES="$PACKAGES bash curl luci luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES dawn luci-app-dawn luci-i18n-dawn-zh-cn"
 #24.10.0
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
